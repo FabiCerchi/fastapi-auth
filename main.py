c4da25a14912feac3e884a8ada4c04b5c97a1d20
Fabi Cerchi <52931746+FabiCerchi@users.noreply.github.com>
2024-12-24 16:42:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import uvicorn
 from app.routers import user, auth
 from core.config import settings
 app = FastAPI(title="API", version="0.1.0")
 
+@app.get("/")
+def index():
+    return RedirectResponse("/docs", status_code=308(
 app.include_router(user.router)
 app.include_router(auth.router)
 
+
+
 # Configuración para Railway
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))  # Usa el puerto asignado por Railway
-    uvicorn.run(app, host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#    import uvicorn
+#    port = int(os.environ.get("PORT", 8000))  # Usa el puerto asignado por Railway
+#    uvicorn.run(app, host="0.0.0.0", port=port)
