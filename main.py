231c45bf37384140398ee5029e0454639bf2d0b3
FabiCerchi <angelfcerchi@gmail.com>
2024-12-24 20:11:24 +0100

    [UPD]: Se comenta run app main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI
 import uvicorn
 from app.routers import user, auth
 from core.config import settings
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(title="API", version="0.1.0")
 
 app.include_router(user.router)
 app.include_router(auth.router)
 
-if __name__ == "__main__":
-
-    uvicorn.run(app, host="localhost", port=8000, reload=True)
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="localhost", port=8000, reload=True)
