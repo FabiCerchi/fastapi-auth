15788ca5b08e608a7a9f1c5b223970ff685df5f5
Fabi Cerchi <52931746+FabiCerchi@users.noreply.github.com>
2024-12-24 16:35:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ app = FastAPI(title="API", version="0.1.0")
 app.include_router(user.router)
 app.include_router(auth.router)
 
-#if __name__ == "__main__":
-#    uvicorn.run(app, host="localhost", port=8000, reload=True)
+# Configuración para Railway
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Usa el puerto asignado por Railway
+    uvicorn.run(app, host="0.0.0.0", port=port)
